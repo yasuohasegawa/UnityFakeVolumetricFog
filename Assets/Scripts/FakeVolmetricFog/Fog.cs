1414e475280143ae8fdb5ce3939ee10cfadcf237
win_yasohasegawa <dev@yasuohasegawa.com>
2018-11-29 17:33:14 +0900

    Initial commit.

---
+++ Assets/Scripts/FakeVolmetricFog/Fog.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Fog : MonoBehaviour {
+    const int BLOCK_SIZE = 16;
+
+    public Mesh mesh;
+    public Material material;
+    public ComputeShader computeShader;
+
+    private Bounds bounds;
+    private ComputeBuffer positionBuffer;
+    private ComputeBuffer rotationMatrixBuffer;
+    private ComputeBuffer argsBuffer;
+    private uint[] args = new uint[5];
+    private uint numeberOfQuads = 60000;
+
+    private Vector3[] positions;
+    private Vector3 maxPos = new Vector3(150f, 1f, 150f);
+
+
+    void Awake()
+    {
+        
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        // 12 = float (4 byte x 3）
+        positionBuffer = new ComputeBuffer((int)numeberOfQuads, 12);
+        rotationMatrixBuffer = new ComputeBuffer((int)numeberOfQuads, Marshal.SizeOf(typeof(Matrix4x4)));
+        positions = new Vector3[numeberOfQuads];
+
+        Matrix4x4[] mat = new Matrix4x4[numeberOfQuads];
+        for (int i = 0; i < numeberOfQuads; i++)
+        {
+            positions[i] = new Vector3(Random.Range(-maxPos.x, maxPos.x), maxPos.y, Random.Range(-maxPos.z, maxPos.z));
+            mat[i] = Matrix4x4.identity;
+        }
+
+        positionBuffer.SetData(positions);
+        rotationMatrixBuffer.SetData(mat);
+        material.SetBuffer("PositionBuffer", positionBuffer);
+        material.SetBuffer("RotationMatrixBuffer", rotationMatrixBuffer);
+
+        bounds = new Bounds(Vector3.zero, new Vector3(numeberOfQuads / 3, numeberOfQuads / 3, numeberOfQuads / 3));
+
+        argsBuffer = new ComputeBuffer(1, args.Length * sizeof(uint), ComputeBufferType.IndirectArguments);
+        args[0] = mesh.GetIndexCount(0);
+        args[1] = numeberOfQuads;
+        args[2] = mesh.GetIndexStart(0);
+        args[3] = mesh.GetBaseVertex(0);
+        args[4] = 0;
+        argsBuffer.SetData(args);
+    }
+
+    /*  // memo
+     *  shader.dispatch(Kernel, 4096, 1, 1)
+
+        and in your compute shader you have
+
+        [numthreads(1024,1,1)]
+
+        That will give up 4096*1024 = 4,194,304 particles in total.
+    */
+    void Update()
+    {
+        int kernelId = computeShader.FindKernel("CSMain");
+        computeShader.SetBuffer(kernelId, "PositionBuffer", positionBuffer);
+        computeShader.SetBuffer(kernelId, "RotationMatrixBuffer", rotationMatrixBuffer);
+
+        int groupSize = Mathf.CeilToInt(numeberOfQuads / BLOCK_SIZE);
+        computeShader.Dispatch(kernelId, groupSize, 1, 1);
+        computeShader.SetFloat("_Time", Time.time);
+        Graphics.DrawMeshInstancedIndirect(mesh, 0, material, bounds, argsBuffer);
+    }
+
+    void OnDestroy()
+    {
+        positionBuffer.Release();
+        rotationMatrixBuffer.Release();
+        argsBuffer.Release();
+    }
+}
---
+++ Assets/Scripts/FakeVolmetricFog/Fog.shader
@@ -0,0 +1,74 @@
+﻿Shader "Custom/Fog" {
+	Properties {
+		_Color ("Color", Color) = (1,1,1,1)
+		_MainTex ("Albedo (RGB)", 2D) = "white" {}
+	}
+	SubShader {
+		Tags{ "Queue" = "Transparent" "RenderType" = "Transparent" "IgnoreProjector" = "True" }
+		LOD 100
+		Cull off
+		ZWrite Off
+		Blend SrcAlpha OneMinusSrcAlpha
+
+		CGPROGRAM
+		#pragma surface surf Lambert alpha vertex:vert
+		#pragma multi_compile_instancing
+		#pragma instancing_options procedural:setup
+
+		sampler2D _MainTex;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+		StructuredBuffer<float3> PositionBuffer;
+		StructuredBuffer<float4x4> RotationMatrixBuffer;
+#endif
+
+		void setup() {
+#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+			float3 verts = PositionBuffer[unity_InstanceID];
+			float x = verts.x;
+			float y = verts.y;
+			float z = verts.z;
+			unity_ObjectToWorld._14_24_34_44 = float4(x, y, z, 1);
+#endif
+		}
+
+		fixed4 _Color;
+
+		void vert(inout appdata_full v) {
+#ifdef UNITY_PROCEDURAL_INSTANCING_ENABLED
+			float3 scale = float3(3, 2, 3);
+			float3 verts = PositionBuffer[unity_InstanceID];
+			float4x4 scaleMat = float4x4(
+				scale.x, 0, 0, 0,
+				0, scale.y, 0, 0,
+				0, 0, scale.z, 0,
+				0, 0, 0, 1
+				);
+
+			float4x4 billboardMat = UNITY_MATRIX_V;
+			billboardMat._m03 =
+				billboardMat._m13 =
+				billboardMat._m23 =
+				billboardMat._m33 = 0;
+
+			float4x4 rotMat = RotationMatrixBuffer[unity_InstanceID];
+			v.vertex.xyz = mul(v.vertex.xyz, rotMat);
+			
+			float3 vert = mul(v.vertex.xyz, billboardMat);
+			v.vertex.xyz = mul(vert, scaleMat);
+#endif
+		}
+
+		void surf(Input IN, inout SurfaceOutput o) {
+			fixed4 res = tex2D(_MainTex, IN.uv_MainTex)*_Color;
+			o.Albedo = res.rgb;
+			o.Alpha = res.a*0.1;
+		}
+		ENDCG
+	}
+	FallBack "Diffuse"
+}
